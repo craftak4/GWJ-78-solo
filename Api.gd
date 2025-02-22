f8b30168b44122d2ff0769214ce9cf94f6d82fba
craftak4 <craftak4@seznam.cz>
2025-02-22 13:24:38 +0100

    idk

--- Api.gd
+++ Api.gd
@@ -5,11 +5,13 @@ var name:String
 var uri:String
 var headers:PackedStringArray = []
 var method:Method = 0
+var data:String
 
-func _init(_uri:String,_headers:PackedStringArray,_method:Method,msgParams:Callable) -> void:
+func _init(_uri:String,_headers:PackedStringArray,_method:Method,_data:String,msgParams:Callable) -> void:
 	uri = _uri
 	headers = _headers
 	method = _method
+	data = _data
 
 func Request(params:Array[String],rq:HTTPRequest):
 	var msg = request.call(params,self)
